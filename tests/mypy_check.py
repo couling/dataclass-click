c0bb7891012378beef1ef7f276455e014dd8c250
Philip Couling <couling@gmail.com>
2024-11-03 09:59:38 +0000

    Extended MyPy test

--- tests/mypy_check.py
+++ tests/mypy_check.py
@@ -28,6 +28,10 @@ class Config:
     bob: Annotated[str | None, option("--bonno")]
 
 
+class Config2:
+    a: Annotated[int, option()]
+
+
 @click.command()
 @dataclass_click(Config)
 def main_a(config: Config):
@@ -38,4 +42,25 @@ def main_a(config: Config):
 @dataclass_click(Config, kw_name="a")
 @click.option("--b")
 def main_b(b: str | None, a: Config):
+    ...
+
+
+@click.command()
+@dataclass_click(Config, kw_name="a")
+@dataclass_click(Config2, kw_name="b")
+def main_c(a: Config, b: Config2):
+    ...
+
+
+@click.command()
+@dataclass_click(Config)
+@dataclass_click(Config2, kw_name="b")
+def main_d(a: Config, b: Config2):
+    ...
+
+
+@click.command()
+@dataclass_click(Config2, kw_name="b")
+@dataclass_click(Config)
+def main_e(a: Config, b: Config2):
     ...
